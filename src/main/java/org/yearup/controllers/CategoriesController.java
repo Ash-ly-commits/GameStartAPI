ee47f7c83171bddac50562f9189c6ea58f0f6d0a
Ash <mendezashleyy@gmail.com>
2025-12-17 16:32:19 -0500

    first commit

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,76 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
+
+public class CategoriesController
+{
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(ProductDao productDao, CategoryDao categoryDao)
+    {
+        this.productDao = productDao;
+        this.categoryDao = categoryDao;
+    }
+
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        return null;
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+        return null;
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return null;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Category addCategory(@RequestBody Category category)
+    {
+        // insert the category
+        return null;
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteCategory(@PathVariable int id)
+    {
+        // delete the category by id
+    }
+}
